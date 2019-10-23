d3c0804642074fc2cbb88e9f7c9e0db5084747ec
Bill Grveles <bill.grv@hotmail.com>
2019-10-23 07:15:04 -0500

    Updated framework.

---
+++ DemoProject/DemoFramework.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework
+{
+    public enum BrowserType
+    {
+        CHROME,
+        IE,
+        FIREFOX
+    }
+
+    //This abstract class is meant to be inherited by a class in which the test case will be run
+    [TestFixture]
+    public abstract class TestCore
+    {
+        BrowserType browser;    //the browser we'll be using for the test
+        string baseURL;         //the starting url
+
+        /*This constructor is meant to be set in inherited classes, which will setup the environment for the test case.
+         Example:
+         public Foo():base (BrowserType.CHROME, "http://www.google.com")  { }
+         */
+        public TestCore(BrowserType type, string url)
+        {
+            browser = type;
+            baseURL = url;
+        }
+        [OneTimeSetUp]
+        public void startTest()
+        {
+            Browsers.Init(browser, baseURL);
+        }
+
+        [OneTimeTearDown]
+        public void endTest()
+        {
+            Browsers.Close();
+        }
+        
+        //This class is for handling browser management.
+        public class Browsers
+        {
+            private static IWebDriver webDriver;
+            
+            public static void Init(BrowserType browser, string baseURL)
+            {
+                switch (browser)
+                {
+                    case BrowserType.CHROME:
+                        webDriver = new ChromeDriver();
+                        break;
+                    case BrowserType.IE:
+                        webDriver = new InternetExplorerDriver();
+                        break;
+                    case BrowserType.FIREFOX:
+                        webDriver = new FirefoxDriver();
+                        break;
+                }
+                webDriver.Manage().Window.Maximize();
+                Goto(baseURL);
+            }
+            public static string Title
+            {
+                get { return webDriver.Title; }
+            }
+            public static IWebDriver getDriver
+            {
+                get { return webDriver; }
+            }
+            public static void Goto(string url)
+            {
+                webDriver.Url = url;
+            }
+            public static void Close()
+            {
+                webDriver.Quit();
+            }
+        }
+
+    }
+}
--- DemoProject/Program.cs
+++ DemoProject/Program.cs
@@ -1,10 +1,48 @@
 ﻿using System;
-using Demo;
+using TestFramework;
+using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace DemoProject
 {
-    class Program
+    public class Program : TestCore
     {
+        public Program():base (BrowserType.CHROME, "https://qxf2.com/selenium-tutorial-main") { }
+
+        [Test]
+        public void Qxf2Test()
+        {
+            Assert.IsTrue(Browsers.getDriver.Title.Contains("Qxf2"));
+
+            IWebElement formName = Browsers.getDriver.FindElement(By.XPath("//*[@id='name']"));
+            formName.SendKeys("abc123");
+
+            IWebElement formEmail = Browsers.getDriver.FindElement(By.XPath("//*[@id='myform']/div[2]/input"));
+            formEmail.SendKeys("def456@ghi789.com");
+
+            IWebElement formPhoneNumber = Browsers.getDriver.FindElement(By.XPath("//*[@id='phone']"));
+            formPhoneNumber.SendKeys("1234567890");
+
+            IWebElement formCheckbox = Browsers.getDriver.FindElement(By.XPath("//input[@type='checkbox']"));
+            formCheckbox.Click();
+
+            IWebElement formGender = Browsers.getDriver.FindElement(By.XPath("//button[@data-toggle='dropdown']"));
+            formGender.Click();
+            IWebElement selectMale = Browsers.getDriver.FindElement(By.XPath("//a[text()='Male']"));
+            selectMale.Click();
+            formGender.Click();
+            IWebElement selectFemale = Browsers.getDriver.FindElement(By.XPath("//a[text()='Female']"));
+            selectFemale.Click();
+
+            //Uncomment for visual confirmation before submit
+            //System.Threading.Thread.Sleep(3000);
+
+            IWebElement formSubmit = Browsers.getDriver.FindElement(By.XPath("//*[@id='myform']/p/button"));
+            formSubmit.Click();
+
+            //Uncomment for visual confirmation after submit
+            //System.Threading.Thread.Sleep(3000);
+        }
         
     }
 }
